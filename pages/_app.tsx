b3789503aa43a08e92dfe4cb795624f415f40e5f
Michael Cowan <93941471+mgm-mcowan@users.noreply.github.com>
2022-12-18 23:56:21 -0500

    test: pageshow event responsiveness in _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,21 @@
 import '../styles/globals.css'
+import React from 'react';
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
+  React.useEffect(() => {
+    const handlePageShowEvent = (event: any) => {
+      event?.preventDefault();
+      if (event.persisted) {
+        console.log("@@@ - Page was PERSISTED - SHOULD RELOAD.");
+        // location.reload();
+      }
+    }
+    window.addEventListener('pageshow', handlePageShowEvent);
+    return () => {
+      window.removeEventListener('pageshow', handlePageShowEvent);
+    }
+  }, []);
+
   return <Component {...pageProps} />
 }
